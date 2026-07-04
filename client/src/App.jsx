451ba3904ee3cf2197f8e43ffeef41ff78521fa3
Gaurav Kalal <gauravgen2025@gmail.com>
2026-07-04 19:05:11 +0530

    Initial project setup with React and Express

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <div>
+      <h1>CF Compass</h1>
+
+      <p>
+        Your Competitive Programming Analytics Platform
+      </p>
+
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+function SearchBar() {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter Codeforces Handle"
+      />
+
+      <button>
+        Analyze
+      </button>
+    </div>
+  );
+}
+
+export default SearchBar;
